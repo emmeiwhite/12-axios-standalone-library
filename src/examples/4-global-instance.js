3563ba66e8fcd991df786182a4b8a8fa583d8285
emmeiwhite <emmeiwhite@gmail.com>
2024-10-27 04:51:23 +0530

    created global.js file to use default headers with axios

--- src/examples/4-global-instance.js
+++ src/examples/4-global-instance.js
@@ -1,17 +1,18 @@
-import { useEffect } from 'react';
+import { useEffect } from 'react'
 
-const productsUrl = 'https://www.course-api.com/react-store-products';
-const randomUserUrl = 'https://randomuser.me/api';
+const productsUrl = 'https://www.course-api.com/react-store-products'
+const randomUserUrl = 'https://randomuser.me/api'
 
 const GlobalInstance = () => {
   const fetchData = async () => {
-    console.log('global axios instance');
-  };
+    console.log('global axios instance')
+  }
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
 
-  return <h2 className='text-center'>global instance</h2>;
-};
-export default GlobalInstance;
+  return <h2 className="text-center">global instance</h2>
+}
+
+export default GlobalInstance
