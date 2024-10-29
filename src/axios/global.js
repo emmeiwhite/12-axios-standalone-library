bf95274d33cb62d4fbf661e26eaf157b17862eec
emmeiwhite <emmeiwhite@gmail.com>
2024-10-29 20:18:33 +0530

    Testing the default headers with : axios.defaults.headers['Accept]='text/plain'

--- src/axios/global.js
+++ src/axios/global.js
@@ -2,3 +2,4 @@ import axios from 'axios'
 
 // default request headers for all the requests !!!
 axios.defaults.headers['Accept'] = 'application/json'
+axios.defaults.headers['Accept'] = 'text/plain'
